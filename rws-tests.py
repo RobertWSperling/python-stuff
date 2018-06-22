1b14ce11340d0760b9196e9ecb6c368d567e5a53
GEO-RWSperling <rob7sperling@gmail.com>
2018-06-21 20:48:41 -0500

    my first commit of some python stuff.

---
+++ rws-tests.py
@@ -0,0 +1,38 @@
+# a test of a few things
+
+a = "hello there"
+
+print(a.replace("there", "there is"))
+
+print(a.upper())
+
+print(a.lower())
+
+print(a.lstrip("he"))
+
+b = 255
+
+print(b)
+
+print(b.bit_length())
+
+print(b.__float__())
+
+print(b.__str__())
+
+# print(b.__rshift__(2))
+
+# print(b.__lshift__(2))
+
+print(b)
+print(b.__rlshift__(2))
+
+
+
+
+
+
+
+
+
+# print("hello")
